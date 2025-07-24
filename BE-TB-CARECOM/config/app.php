17c99da884bb94da0ca6c01631e99907827a27c4
mhusainh <husainhasbulah@gmail.com>
2025-07-25 04:04:11 +0700

    feat(mail): add Mailjet integration for email services
    - Add Mailjet service provider to bootstrap and app config
    - Configure Mailjet API keys in services.php
    - Add Mailjet mailer transport configuration
    - Include Mailjet package in composer.json

--- BE-TB-CARECOM/config/app.php
+++ BE-TB-CARECOM/config/app.php
@@ -123,4 +123,25 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'providers' => [
+        Mailjet\LaravelMailjet\MailjetServiceProvider::class,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Class Aliases
+    |--------------------------------------------------------------------------
+    |
+    | This array of class aliases will be registered when this application
+    | is started. However, feel free to register as many as you wish as
+    | the aliases are "lazy" loaded so they don't hinder performance.
+    |
+    */
+
+    'aliases' => [
+
+        'Mailjet' => Mailjet\LaravelMailjet\Facades\Mailjet::class,
+
+    ],
+
 ];
